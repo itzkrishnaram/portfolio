d933b804124af9f285797251385727477283681a
itzkrishnaram <ksaravanakumar@hawk.iit.edu>
2025-02-16 21:21:12 -0600

    test

--- components/About.tsx
+++ components/About.tsx
@@ -12,9 +12,7 @@ const About = () => {
     >
       <h2 className="text-3xl font-bold mb-4 font-heading">About Me</h2>
       <p className="text-lg text-gray-700 dark:text-gray-300">
-        Master's student in Computer Science with a strong background in machine learning, full-stack development, and a
-        passion for solving complex problems. Experienced in developing innovative solutions and leading technical
-        projects.
+       
       </p>
     </motion.section>
   )
